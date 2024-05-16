f69574dea2af7bcd308d68e2cf27ee683e4078ad
brustcraft458 <brustcraft458@gmail.com>
2024-05-16 18:46:54 +0700

    Product Controler

--- app/Http/Controllers/ProductControler.php
+++ app/Http/Controllers/ProductControler.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductControler extends Controller
@@ -11,11 +12,29 @@ class ProductControler extends Controller
     }
 
     public function read() {
-
+        $products = Product::all();
+        return response()->json([
+            'success' => true,
+            'message' => 'Product List',
+            'data' => $products
+        ], 200);
     }
 
     public function readById($id) {
+        $product = Product::find($id);
 
+        if (!$product) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Product Not Found'
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Product Found',
+            'data' => $product
+        ], 200);
     }
 
     public function update(Request $request, $id) {
