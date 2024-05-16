cd46f14fbe429806384085f647450eaf8dc91def
brustcraft458 <brustcraft458@gmail.com>
2024-05-16 15:56:39 +0700

    update

---
+++ app/Http/Controllers/ProductControler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductControler extends Controller
+{
+    public function create(Request $request) {
+
+    }
+
+    public function read() {
+
+    }
+
+    public function readById($id) {
+
+    }
+
+    public function update(Request $request, $id) {
+
+    }
+
+    public function delete($id) {
+
+    }
+
+
+}
